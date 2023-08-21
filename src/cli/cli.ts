3431e0d05e9b20d2fb287a4bf8aa9868517217ab
Guilherme Chaguri <guichaguri@gmail.com>
2023-08-21 04:29:40 -0300

    feat(cli): added help after error

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -9,6 +9,7 @@ program
   .name('racing-data-converter')
   .description('CLI to convert racing data into formats from popular data analysis software')
   .version('1.0.0')
+  .showHelpAfterError()
   .argument('<input>', 'input file path')
   .argument('<output>', 'output file path')
   .addOption(
