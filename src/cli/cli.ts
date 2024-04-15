9ade180f3ef1ad3eb48ed635d742f1a146508314
Guilherme Chaguri <guichaguri@gmail.com>
2024-04-15 06:59:43 -0300

    bump(version): bump version to 1.2.0

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('racing-data-converter')
   .description('Convert time series data into formats from popular motorsport data analysis software')
-  .version('1.1.1')
+  .version('1.2.0')
   .showHelpAfterError()
   .argument('<input>', 'input file path')
   .argument('<output>', 'output file path')
